a253b8662c1477a44bccf2576efd50c1992f8fe0
vino <albertusdavino@gmail.com>
2020-07-10 00:31:34 +0700

    Initial Commit

---
+++ time.py
@@ -0,0 +1,21 @@
+import time
+
+# start = time.time()
+
+
+# print('loading list-')
+# print('loading list--')
+# print('loading list---')
+# time.sleep(3)
+# print('loading list-')
+# print('loading list--')
+# print('loading list---')
+# time.sleep()
+# print('loading list-')
+# print('loading list--')
+# print('loading list---')
+
+
+# stop = time.time()
+# print(stop-start)
+
